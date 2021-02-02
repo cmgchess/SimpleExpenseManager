58d3ef7618d2609d195b96e76398283022f47a6d
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-02-02 12:03:46 +0530

    PersistentTransactionDAO implementation

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -0,0 +1,36 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+import android.content.Context;
+
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+
+public class PersistentTransactionDAO implements TransactionDAO {
+    private DBHelper dbHelper;
+
+    public PersistentTransactionDAO(Context context){
+        dbHelper = new DBHelper(context);
+    }
+
+    @Override
+    public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
+        Transaction transaction = new Transaction(date,accountNo,expenseType,amount);
+        if (accountNo!=null){
+            dbHelper.logTransaction(transaction);
+        }
+    }
+
+    @Override
+    public List<Transaction> getAllTransactionLogs() {
+        return dbHelper.getAllTransactionLogs(-1); //-1 defined to get all
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        return dbHelper.getAllTransactionLogs(limit);
+    }
+}
